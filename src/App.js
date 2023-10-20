d054f1c73945c0a048c6de5164922b326713509b
Trixxzzz <ramtekearyan2003@gmail.com>
2023-10-20 21:19:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,171 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import {
+  Box,
+  Button,
+  Container,
+  HStack,
+  Input,
+  VStack,
+} from "@chakra-ui/react";
+import Message from "./Components/Message";
+import { app } from "./Firebase";
+import {
+  onAuthStateChanged,
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import {
+  getFirestore,
+  addDoc,
+  collection,
+  serverTimestamp,
+  onSnapshot,
+  query,
+  orderBy,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const loginHandler = () => {
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(auth, provider);
+};
+
+const logoutHandler = () => signOut(auth);
+
+// Function to delete a message by its document ID
+const deleteMessage = async (messageId) => {
+  try {
+    await deleteDoc(doc(db, "Messages", messageId));
+  } catch (error) {
+    alert(error);
+  }
+};
 
 function App() {
+  const [user, setUser] = useState(false);
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const divForScroll = useRef(null);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      setMessage("");
+
+      await addDoc(collection(db, "Messages"), {
+        text: message,
+        uid: user.uid,
+        uri: user.photoURL,
+        createdAt: serverTimestamp(),
+      });
+
+      divForScroll.current.scrollIntoView({ behavior: "smooth" });
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  useEffect(() => {
+    const q = query(collection(db, "Messages"), orderBy("createdAt", "asc"));
+
+    const unsubscribe = onAuthStateChanged(auth, (data) => {
+      setUser(data);
+    });
+
+    const unsubscribeForMessage = onSnapshot(q, (snap) => {
+      setMessages(
+        snap.docs.map((item) => {
+          const id = item.id;
+          return { id, ...item.data() };
+        })
+      );
+    });
+
+    return () => {
+      unsubscribe();
+      unsubscribeForMessage();
+    };
+  }, []);
+
+  // Function to delete a message
+  const handleDeleteMessage = async (messageId) => {
+    await deleteMessage(messageId);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box bg={"red.100"}>
+      {user ? (
+        <Container h={"100vh"} bg={"white"}>
+          <VStack h="full" paddingY={"4"}>
+            <Button onClick={logoutHandler} colorScheme={"red"} w={"full"}>
+              Logout
+            </Button>
+
+            <VStack
+              h="full"
+              w={"full"}
+              overflowY="auto"
+              css={{
+                "&::-webkit-scrollbar": {
+                  display: "none",
+                },
+              }}
+            >
+              {messages.map((item) => (
+                <Message
+                  key={item.id}
+                  user={item.uid === user.uid ? "me" : "other"}
+                  text={item.text}
+                  uri={item.uri}
+                >
+                  {/* Include a delete button and handle the delete action */}
+                  {user.uid === item.uid && (
+                    <Button
+                      size="sm"
+                      colorScheme="red"
+                      onClick={() => handleDeleteMessage(item.id)}
+                    >
+                      Delete
+                    </Button>
+                  )}
+                </Message>
+              ))}
+
+              <div ref={divForScroll}></div>
+            </VStack>
+
+            <form onSubmit={submitHandler} style={{ width: "100%" }}>
+              <HStack>
+                <Input
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                  placeholder={"Enter a Message..."} 
+                  textColor={"black"} 
+                  _placeholder={{ color: 'black' }}
+                />
+                <Button colorScheme={"purple"} type="submit">
+                  Send
+                </Button>
+              </HStack>
+            </form>
+          </VStack>
+        </Container>
+      ) : (
+        <VStack bg="white" justifyContent={"center"} h="100vh">
+          <Button onClick={loginHandler} colorScheme={"purple"}>
+            Sign In With Google
+          </Button>
+        </VStack>
+      )}
+    </Box>
   );
 }
 
---
+++ src/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBOOLm2H116YDA98pbgPyNcxM-yaGl2Uqw",
+  authDomain: "chatme-73930.firebaseapp.com",
+  projectId: "chatme-73930",
+  storageBucket: "chatme-73930.appspot.com",
+  messagingSenderId: "906494473590",
+  appId: "1:906494473590:web:e6b0b88dab06139cc23657"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
